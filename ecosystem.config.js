9a14b45f4d8eaca3590ac0f005d70963a4b7d614
PabloRNC <pabloruanonavarro@gmail.com>
2024-12-26 14:38:11 +0100

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,7 @@ module.exports = {
       {
         name: "valtools",
         script: "./dist/index.js",
-        instances: "1",
-        exec_mode: "cluster",
+        exec_mode: "fork",
       },
     ],
   };
