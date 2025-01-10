7286b3ec5743e84fed207189f3668249c48e56fb
PabloRNC <pabloruanonavarro@gmail.com>
2025-01-10 21:45:59 +0100

    updated

--- src/routes/Api.ts
+++ src/routes/Api.ts
@@ -158,6 +158,8 @@ Api.get(
 
 Api.get("/players/:channelId", async ({ headers, set, params: { channelId } }) => {
 
+  console.log(channelId);
+
   const payload = isAuthorized(headers);
 
   if(!payload){
